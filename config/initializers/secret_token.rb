2db46194e26865de0a66d19b3ec905d66747fa42
Doug Gerber <dojoh@outlook.com>
2013-08-17 19:00:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlackerNews::Application.config.secret_key_base = 'bda09f68b1f8704accf88d7cde5a1ce0878ae667befd57af5ed27918cfc7b866736286da83f5c6844bf5e9ef664ac862847ebb7633377b4355e312142c2a4a2a'
